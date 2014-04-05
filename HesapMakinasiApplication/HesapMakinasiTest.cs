fb99a03b229aa5f2f48bd7d4139e0deb4ee48303
aliulug <keremaliulug@gmail.com>
2014-04-05 04:18:49 +0300

    .

---
+++ HesapMakinasiApplication/HesapMakinasiTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace HesapMakinasiApplication
+{
+	[TestFixture]
+	public class HesapMakinasiTest
+	{
+		[Test]
+		public void IkiVeUcToplanirsa_BesDoner()
+		{
+			HesapMakinasi hesapMakinasi = new HesapMakinasi();
+			int sonuc = hesapMakinasi.Topla(2, 3);
+			Assert.That(sonuc, Is.EqualTo(5));
+		}
+
+		[Test]
+		public void DortVeYediToplanirsa_OnbirDoner()
+		{
+			HesapMakinasi hesapMakinasi = new HesapMakinasi();
+			int sonuc = hesapMakinasi.Topla(4, 7);
+			Assert.That(sonuc, Is.EqualTo(11));
+		}
+	}
+
+	public class HesapMakinasi
+	{
+		public int Topla(int sayi1, int sayi2)
+		{
+			return sayi1 + sayi2;
+		}
+	}
+}
